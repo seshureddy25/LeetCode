3103650d72ad5fb94aa0412c08cb21ff21c36819
VEERAMREDDYGARI SESHU KUMAR REDDY <vskr5696@gmail.com>
2026-09-03 07:35:30 +0530

    Time: 0 ms (100%), Space: 15.6 MB (51.4%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.c
@@ -0,0 +1,24 @@
+int maxProfit(int* prices, int size)
+{
+    int buy1 = -prices[0];
+    int sell1 = 0;
+    int buy2 = -prices[0];
+    int sell2 = 0;
+
+    for(int i = 1; i < size; i++)
+    {
+        if(-prices[i] > buy1)
+            buy1 = -prices[i];
+
+        if(buy1 + prices[i] > sell1)
+            sell1 = buy1 + prices[i];
+
+        if(sell1 - prices[i] > buy2)
+            buy2 = sell1 - prices[i];
+
+        if(buy2 + prices[i] > sell2)
+            sell2 = buy2 + prices[i];
+    }
+
+    return sell2;
+}
